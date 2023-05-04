510773a438cf41a88a50d9c8f664c591968d9402
Geno <davidgeno55@gmail.com>
2023-05-04 14:22:24 +0530

    k

--- src/pages/question/Questions.jsx
+++ src/pages/question/Questions.jsx
@@ -6,36 +6,64 @@ import { useNavigate, useParams } from 'react-router-dom';
 const Questions = () => {
 
     const { links } = useSelector(state => state.link)
-    console.log(links);
+    // console.log(links);
     const navi = useNavigate()
     const param = useParams()
 
-    const [data, setData] = useState([])
+    // const [data, setData] = useState([])
+    // const [find, setFind] = useState({})
+
+    // useEffect(() => {
+    //     question()
+    //     filtervalues()
+    // }, [find])
+
+    // const question = async () => {
+    //     const getdata = await axios.get(`${links}/admin/get`)
+    //     // console.log(getdata);
+    //     setData(getdata.data)
+    //     // const finddata = data.find(value => value._id == param.id)
+    //     // setFind(finddata)
+    //     // console.log(finddata);
+    // }
+
+    // const filtervalues = () => {
+    //     const finddata = data.find(value => value._id == param.id)
+    //     setFind(finddata)
+    //     console.log(finddata);
+    // }
+    // console.log(find);
+
+    const [activeno, setActiveno] = useState(0)
     const [find, setFind] = useState({})
 
     useEffect(() => {
-        // question()
-        filtervalues()
-    }, [find])
+        ques()
+    }, [])
 
-    const question = async () => {
+    const ques = async () => {
         const getdata = await axios.get(`${links}/admin/get`)
-        console.log(getdata);
-        setData(getdata.data)
-        const finddata = data.find(value => value._id == param.id)
+        // setData(getdata.data)
+        const finddata = getdata.data.find(value => value._id == param.id)
         setFind(finddata)
         console.log(finddata);
     }
+    console.log(find.questions);
 
-    const filtervalues = () => {
-        const finddata = data.find(value => value._id == param.id)
-        setFind(finddata)
-        console.log(finddata);
-    }
-    console.log(find);
-
+    // const { question } = find.questions && find.questions[0]
+    const { question } = find.questions?.[activeno] || {}
+    console.log(question);
     return (
-        <div>{param.id}</div>
+        <div>
+            {/* {find.questions && find.questions.map((values, key) => {
+                return (
+                    <h1 key={key}>{values.question}</h1>
+                )
+            })} */}
+            {/* <h1>{question}</h1> */}
+            {question && <h1>{question}</h1>}
+            <button onClick={() => setActiveno(activeno + 1)}>Next</button>
+        </div>
     )
 }
 
