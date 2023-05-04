517e48e34a8abe3a8f7ac410071277b556707671
Geno <davidgeno55@gmail.com>
2023-05-04 10:25:41 +0530

    first commit

---
+++ src/redux/future/LinkSlice.jsx
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = import.meta.env.VITE_LINK
+
+const linkslice = createSlice({
+    name: link,
+    initialState,
+    reducers: {
+
+    }
+})
+
+export default linkslice
