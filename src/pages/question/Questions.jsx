9bbe8c0668d78c6885da3c7bb473917cc275dedf
Geno <davidgeno55@gmail.com>
2023-05-04 17:01:54 +0530

    k

--- src/pages/question/Questions.jsx
+++ src/pages/question/Questions.jsx
@@ -36,10 +36,19 @@ const Questions = () => {
 
     const [activeno, setActiveno] = useState(0)
     const [find, setFind] = useState({})
+    const [showresult, setShowresult] = useState(false)
+    const [select, setSelect] = useState(null)
+    const [answer, setAnswer] = useState('')
+    const [finish, setFinish] = useState({
+        score: 0,
+        correctanswer: 0,
+        wronganswer: 0
+    })
 
     useEffect(() => {
         ques()
     }, [])
+
 
     const ques = async () => {
         const getdata = await axios.get(`${links}/admin/get`)
@@ -51,18 +60,85 @@ const Questions = () => {
     console.log(find.questions);
 
     // const { question } = find.questions && find.questions[0]
-    const { question } = find.questions?.[activeno] || {}
+    // const { questions } = quizs
+    const { question, choose, correctanswer } = find.questions?.[activeno] || {}
     console.log(question);
+    const selectanswer = (items, key) => {
+        setSelect(key)
+        if (items === correctanswer) {
+            setAnswer(true)
+            console.log(true);
+        }
+        else {
+            setAnswer(false)
+        }
+    }
+    const addbutton = () => {
+        setSelect(null)
+        setFinish((pre) => {
+            answer ? {
+                ...pre,
+                score: pre.score + 1,
+                correctanswer: pre.correctanswer + 1,
+            } : {
+                ...pre,
+                wronganswer: pre.wronganswer + 1
+            }
+        })
+        if (activeno !== question.length - 1) {
+            setActiveno(pre => pre + 1)
+        }
+        else {
+            setActiveno(0)
+            setShowresult(true)
+        }
+        console.log(setShowresult);
+    }
+
     return (
         <div>
-            {/* {find.questions && find.questions.map((values, key) => {
+            {
+                !showresult ? (
+                    <div>
+                        {/* {find.questions && find.questions.map((values, key) => {
                 return (
                     <h1 key={key}>{values.question}</h1>
                 )
             })} */}
-            {/* <h1>{question}</h1> */}
-            {question && <h1>{question}</h1>}
-            <button onClick={() => setActiveno(activeno + 1)}>Next</button>
+                        {/* <h1>{question}</h1> */}
+                        <h1>Quiz</h1>
+                        <h1>{question}</h1>
+                        <ul>
+                            {
+                                choose?.map((items, key) => {
+                                    return (
+                                        <li className={select == key ? 'active' : null} onClick={() => selectanswer(items, key)} key={key}>{items}</li>
+                                    )
+                                })
+                            }
+                        </ul>
+
+                        {/* {
+                activeno === (find.questions?.length == 0) ? <button onClick={() => setActiveno(activeno - 1)}>Back</button> : null
+            } */}
+                        {
+                            activeno == (find.questions?.length > 1) ? <button onClick={() => setActiveno(activeno - 1)}>Back</button> : null
+                        }
+
+                        {
+                            activeno === (find.questions?.length - 1) ? <button onClick={() => setShowresult(true)}>Finish</button> : <button onClick={() => addbutton()}>Next</button>
+                        }
+                        {/* <button onClick={() => addbutton()}>
+                            {
+                                activeno === (find.questions?.length - 1) ? 'finish' : 'Next'
+                            }
+                        </button> */}
+                    </div>
+                ) :
+                    (<div>
+                        <h1>result</h1>
+                    </div>)
+            }
         </div>
     )
 }
